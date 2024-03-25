56ef9a3cd161906390b03dade050accf1fe84356
vinay-winai <vinai2k20@gmail.com>
2024-03-25 19:08:49 +0530

    added robfig/cron,fixed names

--- vinay_konduru_1nm/daily_aggregate_ledger_report/cmd/daily_aggregate_ledger_report/main.go
+++ vinay_konduru_1nm/daily_aggregate_ledger_report/cmd/daily_aggregate_ledger_report/main.go
@@ -3,20 +3,40 @@ package main
 import (
 	"time"
 	"log"
-	lr "github.com/vinay-winai/go_microservices_course_final_project/internal/implementation"
+	"os"
+	"os/signal"
+	"github.com/robfig/cron/v3"
+	reportGenerator "github.com/vinay-winai/go_microservices_course_final_project/internal/implementation"
 )
 
 func main() {
 	defer log.Println("Service crashed")
-	for {
-		time_to_midnight := time.Until(time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day()+1, 0, 0, 0, 0, time.Local))
-		log.Println("Sleeping for", time_to_midnight)
-		time.Sleep(time_to_midnight)
+	cronJob := cron.New()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	dailyPaymentsReport:= func() {
+		log.Println("Running job at", time.Now())
 		log.Println("Processing daily report...")
-		res,err := lr.GenerateDailyAggregateLedgerReport()
+		res,err := reportGenerator.DailyPaymentsReport()
 		log.Println(res)
 		if err != nil {
-			log.Println(err)
+			log.Println("Job failed:", err)
+			quit <- os.Interrupt
+			}
 		}
+	cron_id, err := cronJob.AddFunc("@midnight", dailyPaymentsReport)
+	if err != nil {
+		log.Println("Error scheduling job:", err)
+		return
+	}
+	log.Printf("Scheduled job with ID %d\n", cron_id)
+	cronJob.Start()
+	<-quit
+	jobCtx := cronJob.Stop()
+	log.Println("Waiting for jobs to be done...")
+	<-jobCtx.Done()
+	err = jobCtx.Err()
+	if err != nil {
+		log.Println("Cause of failure:", err)
 	}
 }
